b7d665fa9d39dc465c39158246c5fc226183b8c4
nosignalx2k <kingofsweetsx2k@gmail.com>
2024-10-12 11:42:58 +0300

    add unzip

--- src/download_weights.py
+++ src/download_weights.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import zipfile
 
 def download_weights(url, save_path):
     """
@@ -9,7 +10,6 @@ def download_weights(url, save_path):
         url: URL для скачивания весов.
         save_path: Путь для сохранения весов.
     """
-  
     if os.path.exists(save_path):
         print(f"Файл с весами уже существует: {save_path}")
         return
@@ -21,10 +21,26 @@ def download_weights(url, save_path):
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     
     with open(save_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192): 
+        for chunk in response.iter_content(chunk_size=8192):
             f.write(chunk)
 
     print(f"Веса модели успешно скачаны и сохранены в {save_path}")
+    unzip_weights(save_path)
+
+def unzip_weights(zip_path):
+    """
+    Распаковывает архив с весами в ту же директорию, где находится архив.
+
+    Args:
+        zip_path: Путь до архива с весами.
+    """
+    extract_dir = os.path.dirname(zip_path)
+    print(f"Распаковка архива {zip_path} в {extract_dir}...")
+    
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_dir)
+    
+    print(f"Архив успешно распакован в {extract_dir}")
 
 if __name__ == "__main__":
     weights_url = "https://github.com/deepinsight/insightface/releases/download/v0.7/buffalo_l.zip"  # URL для скачивания весов ResNet50@WebFace600K
