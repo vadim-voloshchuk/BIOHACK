8ee6edfcf67e42aa34655ca6459560f160cb6c75
nosignalx2k <kingofsweetsx2k@gmail.com>
2024-10-12 22:44:45 +0300

    check custon model autoenv

--- get_results.py
+++ get_results.py
@@ -5,9 +5,16 @@ from PIL import Image
 import numpy as np
 import zipfile
 from torch import nn
-from custom_autoenv_level2 import AdvancedImageRestorationModel
+from scipy.spatial.distance import cosine
+from custom_autoenv_level2 import AdvancedImageRestorationModel 
+import onnxruntime as ort
+import torchvision.transforms as transforms
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Инициализация ONNX-сессии
+weights_path = "models/w600k_r50.onnx"
+session = ort.InferenceSession(weights_path)
+
 
 # Путь к pickle-файлу
 pickle_path = "embedding_v3.pickle"
@@ -15,6 +22,8 @@ pickle_path = "embedding_v3.pickle"
 # Путь для сохранения сгенерированных изображений
 output_dir = "imgs"
 os.makedirs(output_dir, exist_ok=True)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 # Инициализация модели и загрузка весов (ваша модель уже определена ранее)
 model = AdvancedImageRestorationModel().to(device)
@@ -25,9 +34,12 @@ model.eval()
 with open(pickle_path, 'rb') as f:
     data = pickle.load(f)
 
+# Инициализация переменной для суммы косинусных расстояний
+total_cosine_distance = 0.0
+
 # Проход по каждому вектору из словаря
 for img_name, embeddings in data.items():
-    embedding = embeddings['embedding']  # Берем embedding
+    embedding = embeddings['embedding']  # Берем исходный embedding
     embedding_tensor = torch.tensor(embedding).float().unsqueeze(0).to(device)  # Преобразуем в тензор
     
     with torch.no_grad():
@@ -40,6 +52,14 @@ for img_name, embeddings in data.items():
         image = Image.fromarray(reconstructed_image.transpose(1, 2, 0))  # Преобразуем в (height, width, channels)
         image.save(os.path.join(output_dir, img_name))
 
+        # Вычисляем вектор для сгенерированного изображения
+        img_tensor = transforms.ToTensor()(image).unsqueeze(0).to(device)  # Преобразуем обратно в тензор
+        generated_vector = session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img_tensor.cpu().numpy()})[0]
+        
+        # Вычисляем косинусное расстояние между исходным и сгенерированным векторами
+        cosine_distance = cosine(embedding, generated_vector[0])
+        total_cosine_distance += cosine_distance
+
 # Создаем zip архив с изображениями
 archive_name = "archive.zip"
 with zipfile.ZipFile(archive_name, 'w') as archive:
@@ -47,3 +67,4 @@ with zipfile.ZipFile(archive_name, 'w') as archive:
         archive.write(os.path.join(output_dir, img_name), arcname=os.path.join('imgs', img_name))
 
 print(f"Архив {archive_name} успешно создан.")
+print(f"Сумма косинусных расстояний: {total_cosine_distance}")
